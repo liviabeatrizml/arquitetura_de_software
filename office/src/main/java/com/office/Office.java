eab8ea022d7c87924a621f10e93ecb93eb499f41
Bazch <mikael.vidal@gmail.com>
2024-08-28 16:17:39 -0300

    Remoção de metodo não utilizado e implementação de novos

--- office/src/main/java/com/office/Office.java
+++ office/src/main/java/com/office/Office.java
@@ -1,8 +1,11 @@
 package com.office;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
 //interface do produto criado, neste caso, arquivo docx, xslx e pptx
 public interface Office {
-    void criar();
     void editarArquivo();
     void excluirArquivo();
+    void criarParagrafo(XWPFDocument document);
+    void gravarDocumento(XWPFDocument document);
 }
