11909fa67fe7d4a73205b1eb0c3e82af6e6d6b2f
Bazch <mikael.vidal@gmail.com>
2024-08-28 16:16:43 -0300

    criação de metodo "criarParagrafo", ajustes de imports

--- office/src/main/java/com/office/Docs.java
+++ office/src/main/java/com/office/Docs.java
@@ -1,21 +1,52 @@
 package com.office;
 
-public class Docs implements Office {
-    
-    @Override
-    public void criar() {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+public class Docs implements Office {
+
+    public static String paragrafo = "paragrafo.txt";
+    public static String output = "arquivoGerado.docx";
+
+    @Override
+    public void criarParagrafo(XWPFDocument document) {
+        String texto = "", op = "";
+        Manipulacao.criarArquivoDeTexto();
+        Manipulacao.escreverNoArquivoTXT(texto);
+        XWPFParagraph para = document.createParagraph();
+        Manipulacao.selecionarAlinhamento(op, para);
+        String textoDoTXT = Manipulacao.convertTextFileToString(paragrafo);
+        XWPFRun paraRun = para.createRun();
+        paraRun.setText(textoDoTXT);
+    }
+
+    public void gravarDocumento(XWPFDocument document) {
+        try (FileOutputStream out = new FileOutputStream(output)) {
+            document.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            document.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void editarArquivo() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'editarArquivo'");
     }
 
     @Override
     public void excluirArquivo() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'excluirArquivo'");
     }
+
 }
