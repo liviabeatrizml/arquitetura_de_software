8bef0dad79b30591578012a3c8b7459b01e4023d
Bazch <mikael.vidal@gmail.com>
2024-08-28 16:17:00 -0300

    Testando novas implementações na main

--- office/src/main/java/com/office/Main.java
+++ office/src/main/java/com/office/Main.java
@@ -1,6 +1,13 @@
 package com.office;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
 public class Main {
     public static void main(String[] args) {
-        
+        OfficeFactory DocsF = new DocsFactory();
+        Office docx = DocsF.criarArquivoOffice();
+        XWPFDocument document = new XWPFDocument();
+        docx.criarParagrafo(document);
+        docx.gravarDocumento(document);
     }
 }
